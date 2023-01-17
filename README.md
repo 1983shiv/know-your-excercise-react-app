52fbbf36f6ac60e0182d5b0a8208da51397b2a79
Shiv Srivastava <1983.shiv@gmail.com>
2023-01-17 19:08:19 +0530

    prepare the first draft of gym / excercises

---
+++ README.md
@@ -0,0 +1,28 @@
+<h1 align="center">👋 Hi, I’m Shiv Srivastava</h1>
+
+<h3 align="center">A Full-Stack developer from India</h3>
+<h3 align="center"><a href="https://twitter.com/const_shiv" ><img src="https://img.shields.io/twitter/follow/const_shiv.svg?style=social" /> </a></h3>
+
+# Know your excercise
+
+### It will provide detail about excercises, body parts, target body parts and equipement used on excercises with userful youtube videos and similar excercises details.
+
+#### Design excercise app using React.js and material-ui and rapid Api to learn how to draw the functional ui using material ui and react.js
+
+#### Check online
+
+`#`
+
+#### want to check on your local computer, just follow commands and you are good to start
+
+clone the repository to your computer
+`git clone https://github.com/1983shiv/know-your-excercise`
+
+go to tik-tac-to directory
+`cd know-your-excercise`
+
+install necessary dependancies
+`npm install`
+
+run the app
+`npm start`
---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import allExcercise from "../data/excercises.json";
+
+import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
+import Detail from "../components/Detail";
+// import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
+
+const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  // const [exerciseVideos, setExerciseVideos] = useState([]);
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setEquipmentExercises] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = allExcercise.filter(
+        (excercise) => excercise.id === id
+      );
+
+      setExerciseDetail(exerciseDetailData[0]);
+      console.log("excercisedetail", exerciseDetail);
+      const targetMuscleExercisesData = allExcercise.filter(
+        (excercise) => excercise.target === exerciseDetail[0].target
+      );
+
+      setTargetMuscleExercises(targetMuscleExercisesData);
+
+      const equimentExercisesData = allExcercise.filter(
+        (excercise) => excercise.equipment === exerciseDetailData[0].equipment
+      );
+      setEquipmentExercises(equimentExercisesData);
+
+      // allExcercise
+      // const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      // setExerciseDetail(exerciseDetailData);
+
+      // const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
+      // setExerciseVideos(exerciseVideosData.contents);
+
+      // const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+      // setTargetMuscleExercises(targetMuscleExercisesData);
+
+      // const equimentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+      // setEquipmentExercises(equimentExercisesData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  if (!exerciseDetail) return <div>No Data</div>;
+
+  return (
+    <Box sx={{ mt: { lg: "96px", xs: "60px" } }}>
+      <Detail exerciseDetail={exerciseDetail} />
+      {/* <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseDetail.name}
+      /> */}
+      <SimilarExercises
+        targetMuscleExercises={targetMuscleExercises}
+        equipmentExercises={equipmentExercises}
+      />
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
